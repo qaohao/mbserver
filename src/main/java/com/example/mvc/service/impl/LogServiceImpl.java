20c3d73c88b2f82689fe90a064b87c288467b148
meng_zhang <meng_zhang@allyes.com>
2015-05-06 10:22:30 +0800

    add LogServiceImpl

--- src/main/java/com/example/mvc/service/impl/LogServiceImpl.java
+++ src/main/java/com/example/mvc/service/impl/LogServiceImpl.java
@@ -1,15 +1,28 @@
 package com.example.mvc.service.impl;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.example.mvc.controller.LogController;
 import com.example.mvc.service.LogService;
 
+@Service
 public class LogServiceImpl implements LogService {
-
+	private static final Logger logger = LoggerFactory
+            .getLogger(LogController.class);
+	
 	@Override
 	public void parse(InputStream is) {
-		// TODO Auto-generated method stub
-
+		try {
+			logger.debug(IOUtils.toString(is));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
